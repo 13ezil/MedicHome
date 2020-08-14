e5bdbe5d3b62c872f1eae565d4bbefd58b39f682
Bezil <thebezil13@gmail.com>
2020-08-14 20:07:46 +0545

    msg

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -3,7 +3,7 @@
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
         <script src="{{ asset('js/app.js') }}" defer></script>
     </head>
-    <body>
+    <body style="background-color: #08AEEA;background-image: linear-gradient(0deg, #08AEEA 0%, #33c76d 90%);">
         @include('include.nav')
         @yield('content')
         @include('include.footer')
